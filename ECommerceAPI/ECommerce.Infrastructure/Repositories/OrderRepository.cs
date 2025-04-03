7a64bcb003f05ac58c01a3715a85c2f068caffc3
bejoluc <blazej.luc@gmail.com>
2025-04-03 21:23:37 +0200

    v221

--- ECommerceAPI/ECommerce.Infrastructure/Repositories/OrderRepository.cs
+++ ECommerceAPI/ECommerce.Infrastructure/Repositories/OrderRepository.cs
@@ -1,40 +1,44 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ECommerceAPI.Data;
-using ECommerce.Application.Interfaces;
+﻿using ECommerce.Application.Interfaces;
 using ECommerce.Domain.Entities;
+using ECommerceAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace ECommerceAPI.Repositories
+namespace ECommerce.Infrastructure.Repositories;
+
+public class OrderRepository : IOrderRepository
 {
-    public class OrderRepository : IOrderRepository
+    private readonly AppDbContext _context;
+
+    public OrderRepository(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public OrderRepository(AppDbContext context)
-        {
-            _context = context;
-        }
+    public async Task<IEnumerable<Order>> GetAllAsync()
+    {
+        return await _context.Orders
+            .Include(o => o.OrderProducts)
+            .ThenInclude(op => op.Product)
+            .ToListAsync();
+    }
 
-        public async Task<IEnumerable<Order>> GetAllAsync()
-        {
-            return await _context.Orders.Include(o => o.Products).ToListAsync();
-        }
+    public async Task<Order?> GetByIdAsync(int id)
+    {
+        return await _context.Orders
+            .Include(o => o.OrderProducts)
+            .ThenInclude(op => op.Product)
+            .FirstOrDefaultAsync(o => o.Id == id);
+    }
 
-        public async Task<Order?> GetByIdAsync(int id)
-        {
-            return await _context.Orders.Include(o => o.Products)
-                                        .FirstOrDefaultAsync(o => o.Id == id);
-        }
+    public async Task AddAsync(Order order)
+    {
+        _context.Orders.Add(order);
+        await _context.SaveChangesAsync();
+    }
 
-        public async Task AddAsync(Order order)
-        {
-            _context.Orders.Add(order);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(Order order)
-        {
-            _context.Orders.Remove(order);
-            await _context.SaveChangesAsync();
-        }
+    public async Task DeleteAsync(Order order)
+    {
+        _context.Orders.Remove(order);
+        await _context.SaveChangesAsync();
     }
 }
